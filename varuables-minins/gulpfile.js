3ee5a8f308fd19f3bda38185c760e55a8dd0205d
goze <goze.qiu@gmail.com>
2017-08-17 14:50:59 +0800

    use postcss-css-variables

---
+++ varuables-minins/gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+var cssnano = require('cssnano');
+var stylelint = require('stylelint');
+var reporter = require('postcss-reporter');
+var cssvariables = require('postcss-css-variables');
+
+gulp.task('style', ['lint'], function () {
+  return gulp.src('src/*.css')
+    .pipe(postcss([autoprefixer, cssvariables(/*options*/)]))
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write('map/'))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('rename', ['style'], function () {
+  return gulp.src('dist/style.css')
+    .pipe(postcss([cssnano]))
+    .pipe(rename('style.min.css'))
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write('map/'))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('lint', function () {
+  return gulp.src('src/*.css')
+    .pipe(postcss([
+      stylelint({
+        "rules": {
+          "color-no-invalid-hex": true,
+          "declaration-colon-space-before": "never",
+          "indentation": 2,
+          "number-leading-zero": "never"
+        }
+      }),
+      reporter({
+        clearAllMessages: true,
+      })
+    ]))
+});
+
+gulp.task('default', ['lint', 'style', 'rename']);
+
+var watcher = gulp.watch('src/*.css', ['default']);
+
+watcher.on('change', function (e) {
+  console.log('File ' + e.path + ' was ' + e.type + ', running tasks...');
+})
---
+++ varuables-minins/readme.md
@@ -0,0 +1,10 @@
+# Run
+```bash
+yarn
+yarn start
+```
+# Reference
+
+[autoprefixer](https://github.com/postcss/autoprefixer)
+[cssnano](https://github.com/ben-eb/cssnano)
+[postcss-css-variables](https://github.com/MadLittleMods/postcss-css-variables)
